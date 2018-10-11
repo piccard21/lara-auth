6b8fd6b16c9a1a9fd1de2b468c778915c51d634e
cafe-serendipity <cafe.serendipity21@gmail.com>
2018-10-11 22:47:43 +0200

    password for api

--- routes/api.php
+++ routes/api.php
@@ -25,3 +25,20 @@
 Route::namespace('Api')->group(function () {
     Route::get('/users', 'UsersController@index');
 });
+
+Route::group([
+    'prefix' => 'auth',
+], function () {
+    Route::post('login', 'Api\AuthController@login');
+    Route::post('signup', 'Api\AuthController@signup');
+
+    Route::group([
+        'middleware' => 'auth:api',
+    ], function () {
+        Route::get('logout', 'Api\AuthController@logout');
+        Route::get('user', 'Api\AuthController@user');
+    });
+});
+
+// http://127.0.0.1:8000/api/auth/login?email=222@aaa.aa&password=222222&remember_me=1
+//

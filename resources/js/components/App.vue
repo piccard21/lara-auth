831c34e04457dbd9830b7c223aaf5951487e6ab5
cafe-serendipity <cafe.serendipity21@gmail.com>
2018-10-14 20:11:34 +0200

    Api & Web

---
+++ resources/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Vue Router Demo App</h1>
+
+        <p>
+            <a href="/logout">Logout</a> |
+            <router-link :to="{ name: 'home' }">Home</router-link> |
+            <router-link :to="{ name: 'hello' }">Hello World</router-link> |
+            <router-link :to="{ name: 'users.index' }">Users</router-link>
+        </p>
+
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+<script>
+    export default {}
+</script>
---
+++ resources/js/components/UsersIndex.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="users">
+        <div class="loading" v-if="loading">
+            Loading...
+        </div>
+
+        <div v-if="error" class="error">
+            <p>{{ error }}</p>
+
+            <p>
+                <button @click.prevent="fetchData">
+                    Try Again
+                </button>
+            </p>
+        </div>
+
+        <ul v-if="users">
+            <li v-for="{ name, email } in users">
+                <strong>Name:</strong> {{ name }},
+                <strong>Email:</strong> {{ email }}
+            </li>
+        </ul>
+
+        <p v-else-if="user"> 
+                <strong>Name:</strong> {{ user.name }},
+                <strong>Email:</strong> {{ user.email }} 
+        </p>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+
+
+export default {
+    data() {
+        return {
+            loading: false,
+            user: null,
+            error: null,
+        };
+    },
+    created() {
+        this.fetchData();
+    },
+    methods: {
+        fetchData() {
+            this.error = this.users = null;
+            this.loading = true;
+            axios
+                .get('/api/auth/user')
+                .then(response => {
+                    this.loading = false;
+                    this.user = response.data;
+                }).catch(error => {
+                    this.loading = false;
+                    this.error = error.response.data.message || error.message;
+                });
+        }
+    }
+}
+</script>
